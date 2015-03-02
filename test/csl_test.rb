60b71755edd6c60c50e674f0e1de630046f1dc16
Robert Jäschke <jaeschke@l3s.de>
2015-03-02 19:29:43 +0100

    test CSL rendering

---
+++ test/csl_test.rb
@@ -0,0 +1,22 @@
+require './test/test_helper'
+require './lib/bibsonomy/csl'
+
+class BibSonomyCSLTest < Minitest::Test
+
+  def setup
+    @csl = BibSonomy::CSL.new(ENV['BIBSONOMY_USER_NAME'], ENV['BIBSONOMY_API_KEY'])
+  end
+  
+  def test_exists
+    assert BibSonomy::CSL
+  end
+  
+  def test_render
+    VCR.use_cassette('render') do
+      html = @csl.render("bibsonomy-ruby", [], 10)
+
+      assert_equal "<h3>2010</h3>", html[0..12]
+      assert_equal "</ul>", html[-6..-2]
+    end
+  end
+end
